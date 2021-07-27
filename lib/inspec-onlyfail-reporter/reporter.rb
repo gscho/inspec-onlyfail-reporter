07d576a53210a96300c6a4309d8b6cefc15ec1dd
gscho <greg.c.schofield@gmail.com>
2021-07-27 15:02:14 -0400

    Filter out everything except fails
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- lib/inspec-onlyfail-reporter/reporter.rb
+++ lib/inspec-onlyfail-reporter/reporter.rb
@@ -3,18 +3,18 @@ require 'inspec/reporters/cli'
 module InspecPlugins::OnlyfailReporter
   class Reporter < Inspec.plugin(2, :reporter)
     def render
-      puts 'here!'
-      run_data[:profiles] = run_data[:profiles].each do |profile|
-        profiles[:controls].each do |control|
-          control.results.select do |result|
-            result.status.eql?('failed')
-          end
+      run_data[:profiles].each do |profile|
+        profile[:controls].select! do |control|
+          control[:results].any? { |res| res.status.eql?('failed') }
         end
       end
-      puts run_data[:profiles].inspect
+      config = {}
+      config[:run_data] = run_data
+      output(Inspec::Reporters::CLI.new(config).render)
     end
+
     def self.run_data_schema_constraints
-      "~> 0.0" # Accept any non-breaking change
+      '~> 0.0'
     end
   end
 end
